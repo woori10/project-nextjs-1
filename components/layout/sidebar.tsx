72316a15764396527b99a04086db123a911e874c
raina.disa <raina.disa@apps.ipb.ac.id>
2026-01-11 14:22:06 +0700

    crud jadwal, dashboard jadwal

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -2,7 +2,6 @@ import { CalendarDaysIcon, DocumentTextIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
 
-
 export default function Sidebar() {
   return (
     <aside className="top-0 left-0 z-40 w-100 h-screen transition-transform -translate-x-full sm:translate-x-0 border-r border-gray-400 bg-white px-6 py-8 shadow-sm">
@@ -13,6 +12,13 @@ export default function Sidebar() {
         </h1> */}
 
         <nav className="flex flex-col mt-28 space-y-5 text-3xl">
+          <Link
+            href="/dashboard"
+            className="flex items-center gap-6 rounded-md px-4 py-3 hover:bg-indigo-50 hover:text-indigo-600 transition"
+          >
+            <CalendarDaysIcon className="w-8 h-8"/>
+            Dashboard
+          </Link>
           <Link
             href="/schedule"
             className="flex items-center gap-6 rounded-md px-4 py-3 hover:bg-indigo-50 hover:text-indigo-600 transition"
@@ -25,7 +31,7 @@ export default function Sidebar() {
             className="flex items-center gap-6 rounded-md px-4 py-3 hover:bg-indigo-50 hover:text-indigo-600 transition"
           >
             <DocumentTextIcon className="w-8 h-8"/>
-            Personal Notes
+            Tugas
           </Link>
           {/* <Link
             href="/task"
