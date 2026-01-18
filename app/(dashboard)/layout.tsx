fe4560aeec3eb7f3c6d036663d59fa300c2808b8
raina.disa <raina.disa@apps.ipb.ac.id>
2026-01-18 22:51:37 +0700

    all done, except black items

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import AppBar from "@/components/layout/appbar";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen bg-gray-50">
       <div className="flex flex-1 flex-col">
         <AppBar />
 
